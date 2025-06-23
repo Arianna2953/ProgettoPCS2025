d9f6d38f5d3cafa69d0cc003b6748b50d2acef9f
Giulia De Santis <ingegneriagiulia@gmail.com>
2025-06-23 21:45:10 +0200

    correzione commenti main e commenti vari

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-#Progetto Programmazione e Calcolo Scientifico 2025
+# Progetto Programmazione e Calcolo Scientifico 2025
+
 Elena Damiani, Giulia De Santis, Arianna Dho
--- main.cpp
+++ main.cpp
@@ -43,8 +43,12 @@ int main(int argc, char* argv[]) {
         v1 = stoi(argv[6]);
 		pathToFind = true;
     }
-
-    cout << "Valori inseriti: p = " << p << ", q = " << q << ", b = " << b << ", c = " << c << ", v0 = " << v0 << ", v1 = " << v1 << endl;
+	cout << "Valori inseriti: p = " << p << ", q = " << q << ", b = " << b << ", c = " << c ;
+	
+	if(pathToFind){
+		cout << ", v0 = " << v0 << ", v1 = " << v1;
+	}	
+	cout << endl;	
     
     string file0Ds;
     string file1Ds;
@@ -62,21 +66,21 @@ int main(int argc, char* argv[]) {
 		file1Ds = "../PlatonicSolid/tetrahedron/Cell1Ds.txt";
 		file2Ds = "../PlatonicSolid/tetrahedron/Cell2Ds.txt";
 		file3Ds = "../PlatonicSolid/tetrahedron/Cell3Ds.txt";
-		cout << "Poliedro regolare di base: Tetraedro" << endl;	
+		cout << "Poliedro regolare di base: Tetraedro (p=3, q=3)" << endl;	
     } 
     else if (p == 3 && q == 4) {
         file0Ds = "../PlatonicSolid/octahedron/Cell0Ds.txt";
 		file1Ds = "../PlatonicSolid/octahedron/Cell1Ds.txt";
 		file2Ds = "../PlatonicSolid/octahedron/Cell2Ds.txt";
 		file3Ds = "../PlatonicSolid/octahedron/Cell3Ds.txt";
-		cout << "Poliedro regolare di base: Ottaedro" << endl;
+		cout << "Poliedro regolare di base: Ottaedro (p=3, q=4)" << endl;
     } 
     else if (p == 3 && q == 5) {
         file0Ds = "../PlatonicSolid/icosahedron/Cell0Ds.txt";
 		file1Ds = "../PlatonicSolid/icosahedron/Cell1Ds.txt";
 		file2Ds = "../PlatonicSolid/icosahedron/Cell2Ds.txt";
 		file3Ds = "../PlatonicSolid/icosahedron/Cell3Ds.txt";
-		cout << "Poliedro regolare di base: Icosaedro" << endl;
+		cout << "Poliedro regolare di base: Icosaedro (p=3, q=5)" << endl;
     } 
     else if (p == 4 && q == 3) {
 		//importo il poliedro di base, triangolo, proietto e poi calcolo il duale del poliedro risultante
@@ -85,9 +89,8 @@ int main(int argc, char* argv[]) {
 		file1Ds = "../PlatonicSolid/octahedron/Cell1Ds.txt";
 		file2Ds = "../PlatonicSolid/octahedron/Cell2Ds.txt";
 		file3Ds = "../PlatonicSolid/octahedron/Cell3Ds.txt";		
-        cout << "Poliedro regolare di base: Ottaedro - bisogna poi farne il duale" << endl;
+        cout << "Poliedro regolare di base: Ottaedro (p=3, q=4)" << endl;
 		dualize = true;
-		cout << "Effettuo la dualizzazione" << endl;
     } 
     else if (p == 5 && q == 3) {
 		//importo il poliedro di base, triangolo, proietto e poi calcolo il duale del poliedro risultante
@@ -96,12 +99,11 @@ int main(int argc, char* argv[]) {
 		file1Ds = "../PlatonicSolid/icosahedron/Cell1Ds.txt";
 		file2Ds = "../PlatonicSolid/icosahedron/Cell2Ds.txt";
 		file3Ds = "../PlatonicSolid/icosahedron/Cell3Ds.txt";		
-        cout << "Poliedro regolare di base: Icosaedro - bisogna poi farne il duale" << endl;	
+        cout << "Poliedro regolare di base: Icosaedro (p=3, q=5)" << endl;	
 		dualize = true;
-		cout << "Effettuo la dualizzazione" << endl;
     } 
     else {
-        cout << "Combinazione di valori di p e q non corrispondente a nessun poliedro regolare." << endl;
+        cerr << "ATTENZIONE - Combinazione di valori di p e q non valida. Nessun poliedro regolare corrispondente." << endl;
 		return 1;
     }
 
@@ -116,12 +118,11 @@ int main(int argc, char* argv[]) {
 	//imposto i vari casi in base al valore di b e c
 	if((b==0 && c >=1) || (b>=1 && c==0)){
 		n = max(b,c);
-		cout << "Triangolazione di 'tipo 1'" << endl;
 		if(dualize == true){
 			TriangulationTypeI(regularPolyhedron, toDualize,q,p,n);
 			cout << "Effettuo la triangolazione di tipo 1" << endl;
 			DualConstructor(toDualize,mesh);
-			cout << "Effettuo la dualizzazione" << endl;
+			cout << "Effettuo la dualizzazione del poliedro triangolato" << endl;
 		}
 		else{
 			TriangulationTypeI(regularPolyhedron, mesh,p,q,n);
@@ -130,12 +131,11 @@ int main(int argc, char* argv[]) {
 	}
 	else if(b==c && b!=0){
 		n = b;
-		cout << "Triangolazione di 'tipo 2'" << endl;
 		if(dualize == true){
 			TriangulationTypeII(regularPolyhedron, toDualize,n);
 			cout << "Effettuo la triangolazione di tipo 2" << endl;
 			DualConstructor(toDualize,mesh);
-			cout << "Effettuo la dualizzazione" << endl;
+			cout << "Effettuo la dualizzazione del poliedro triangolato" << endl;
 		}
 		else{
 			TriangulationTypeII(regularPolyhedron, mesh,n);
@@ -143,14 +143,17 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	else {
-		cout << "valori di b e c non validi" << endl;
+		cerr << "ATTENZIONE - Combinazione di valori b e c non valida" << endl;
 	}
 	
-	//trovo il percorso minimo sul poliedro tra i vertici v0 e v1
-	cout << "CALCOLO DEL CAMMINO MINIMO" << endl;
-	cout << "Id vertice di partenza: " << v0 << endl;
-	cout << "Id vertice d'arrivo: " << v1 << endl;
+	
 	if(pathToFind){
+		
+		//trovo il percorso minimo sul poliedro tra i vertici v0 e v1
+		cout << "CALCOLO DEL CAMMINO MINIMO" << endl;
+		cout << "Id vertice di partenza: " << v0 << endl;
+		cout << "Id vertice d'arrivo: " << v1 << endl;
+		
 		unsigned int numEdges = 0;
 		double pathLenght = 0.0;
 		if(!FindShortestPath(mesh,v0,v1,numEdges,pathLenght)){
